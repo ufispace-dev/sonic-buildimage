88d28ec9f1bf9f46ae94e8cad644051fdf31696f
fengkm <chifeng.km@gmail.com>
2025-10-08 10:38:27 +0800

    Revert "[ufispace][TMP] workaround to fix sonic-slave docker build issue"
    This reverts commit 7ad0f2ddb5e94cdb960838cf456b71827c0f8188.

--- scripts/build_mirror_config.sh
+++ scripts/build_mirror_config.sh
@@ -32,7 +32,6 @@ fi
 
 if [ "$DISTRIBUTION" == "bullseye" ]; then
     DEFAULT_MIRROR_URLS=http://archive.debian.org/debian/
-    DEFAULT_MIRROR_SECURITY_URLS=http://packages.trafficmanager.net/snapshot/debian-security/20250915T001109Z/
 fi
 
 if [ "$MIRROR_SNAPSHOT" == y ]; then
